328b3fa48f29dfc985e69f21cd89467deb93a917
Md. Mehedi Hasan <mehedi17n@gmail.com>
2025-03-17 13:18:31 +0600

    call on back button added

--- features/call/impl/src/main/kotlin/io/element/android/features/call/impl/ui/ElementCallActivity.kt
+++ features/call/impl/src/main/kotlin/io/element/android/features/call/impl/ui/ElementCallActivity.kt
@@ -241,6 +241,7 @@ class ElementCallActivity :
                 .setFeatureFlag("toolbox.alwaysVisible", false)
                 .setFeatureFlag("reactions.enabled", false)
                 .setFeatureFlag("chat.enabled", false)
+                .setFeatureFlag("call-integration.enabled", true)
                 .build()
 
             JitsiMeetActivity.launch(context, options)
