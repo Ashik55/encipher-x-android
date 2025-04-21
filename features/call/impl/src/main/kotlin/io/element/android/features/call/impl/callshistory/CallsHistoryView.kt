17323e215bdbddac796c7656cbd20bc36d96b386
Md. Mehedi Hasan <mehedi17n@gmail.com>
2025-04-21 19:21:19 +0600

    change call logs UI

--- features/call/impl/src/main/kotlin/io/element/android/features/call/impl/callshistory/CallsHistoryView.kt
+++ features/call/impl/src/main/kotlin/io/element/android/features/call/impl/callshistory/CallsHistoryView.kt
@@ -285,7 +285,11 @@ fun CallItem(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = if(call.room_name == null) getDisplayNamesString(call.receiver_display_names) else call.room_name!!,
+                    text = if(call.is_caller == true){
+                        if(call.room_name == null) getDisplayNamesString(call.receiver_display_names) else call.room_name
+                    } else {
+                        if(call.room_name == null) call.caller_display_name.toString() else call.room_name
+                    },
                     style = MaterialTheme.typography.titleMedium,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
